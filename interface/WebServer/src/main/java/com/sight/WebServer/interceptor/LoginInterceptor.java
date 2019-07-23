44b5fdff30283c640cd9d55674511af9d016fbae
deploy.sh <>
2019-07-24 00:36:13 +0800

    login interceptor

--- interface/WebServer/src/main/java/com/sight/WebServer/interceptor/LoginInterceptor.java
+++ interface/WebServer/src/main/java/com/sight/WebServer/interceptor/LoginInterceptor.java
@@ -5,7 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
- 
+
+import net.sf.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -16,7 +22,23 @@ public class LoginInterceptor implements HandlerInterceptor{
     @Override
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         LOG.info("login preHandler");
+    	InputStream inputStreamObject = httpServletRequest.getInputStream();
+        BufferedReader streamReader = new BufferedReader(new InputStreamReader(inputStreamObject, "UTF-8"));
+        StringBuilder responseStrBuilder = new StringBuilder();
+        String inputStr;
+        while ((inputStr = streamReader.readLine()) != null)
+            responseStrBuilder.append(inputStr);
+        JSONObject jsonObject = JSONObject.fromObject(responseStrBuilder.toString());
+        /*
+    	"token": login token,
+    	"id": Account id,
+         */
+        String token = jsonObject.getString("token");
+        String id = jsonObject.getString("id");
+        if(true) // valid token
         return true;
+        else
+        return false;
     }
  
     @Override
