27563f9e1fab5771c1a1f0bd8aee256de8cc786b
deploy.sh <>
2019-08-06 14:46:36 +0800

    ahhhhhhhhhhhhh

--- interface/WebServer/src/main/java/com/sight/WebServer/WebServerApplication.java
+++ interface/WebServer/src/main/java/com/sight/WebServer/WebServerApplication.java
@@ -2,10 +2,16 @@ package com.sight.WebServer;
 
 import javax.annotation.PostConstruct;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.RedisSerializer;
 
@@ -28,4 +34,31 @@ public class WebServerApplication {
 		redisTemplate.setKeySerializer (stringSerializer);
 		redisTemplate .setHashKeySerializer(stringSerializer);
 	}
+	
+    @Bean
+    public Connector connector(){
+        Connector connector=new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector){
+        TomcatServletWebServerFactory tomcat=new TomcatServletWebServerFactory(){
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint=new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection=new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(connector);
+        return tomcat;
+    }
 }
--- interface/WebServer/src/main/java/com/sight/WebServer/api/ResourceController.java
+++ interface/WebServer/src/main/java/com/sight/WebServer/api/ResourceController.java
@@ -8,14 +8,22 @@ import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
 
+import org.apache.tomcat.jni.FileInfo;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.mongodb.gridfs.GridFsTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.mongodb.gridfs.GridFS;
+import com.mongodb.gridfs.GridFSDBFile;
 import com.mongodb.gridfs.GridFSFile;
 import com.mongodb.gridfs.GridFSInputFile;
 import com.sight.WebServer.data.dao.users_filesSqlProvider;
@@ -29,19 +37,23 @@ import net.sf.json.JSONObject;
 public class ResourceController {
     @Autowired
     private GridFsTemplate gridFsTemplate;
-    @RequestMapping(value = "/upload")
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
     @ResponseBody
     public Map<String, Object> uploadFile(HttpServletRequest request) throws Exception {
     	//JSONObject request_para = JSONObject.request.getInputStream().
     	Map<String,Object> ret = new HashMap<String,Object>();
     	JSONObject jsonObject = General.getRequest(request.getInputStream());
+    	Part part = request.getPart("file");
+        String fileName = part.getSubmittedFileName();
+        InputStream ins = part.getInputStream();
+        String contentType = part.getContentType();
+        
         try {
 	        JSONObject parameters = JSONObject.fromObject(jsonObject.get("parameters"));
-	        String file_type = parameters.getString("file_type");
 	        String user_id = parameters.getString("id");
 	        String file_data = parameters.getString("file_data");
 	        String file_name = user_id + General.RandomPostfix();
-	        gridFsTemplate.store(new ByteArrayInputStream(file_data.getBytes()),file_data, user_id + General.RandomPostfix(), file_type);
+	        ObjectId gridFSFile = gridFsTemplate.store(ins, file_name, contentType);
 			/*
 				"file_type": img/raw/...,
 			    "file_data": Stringifyed file data,
@@ -52,7 +64,7 @@ public class ResourceController {
 	        users_files UsersFiles = new users_files();
 	        UsersFiles.setId(file_name);
 	        UsersFiles.setPid(user_id);
-	        UsersFiles.setFileType(file_type);
+	        UsersFiles.setFileType(contentType);
 	        UsersFilesSqlProvider.insertSelective(UsersFiles);
 	        
         }catch(Exception ex) {
@@ -60,15 +72,34 @@ public class ResourceController {
         	ret.put("error_msg", ex.getMessage());
         	return ret;
         }
-    	
 		ret.put("error", 0);
 		return ret;
+
     }
-    @RequestMapping(value = "/resource")
+    @SuppressWarnings("deprecation")
+	@RequestMapping(value = "/resource")
     @ResponseBody
-    public Map<String, Object> resource_get(HttpServletRequest request) throws Exception {
+    public void resource_get(HttpServletRequest request, HttpServletResponse response) throws Exception {
     	Map<String,Object> ret = new HashMap<String,Object>();
-    	ret.put("error", 0);
-    	return ret;
+    	String fileId = request.getParameter("file_id");
+        Query query = Query.query(Criteria.where("_id").is(fileId));
+        
+        com.mongodb.client.gridfs.model.GridFSFile gfsfile = gridFsTemplate.findOne(query);
+        if (gfsfile == null) {
+           
+            return ;
+        }
+        String fileName = gfsfile.getFilename().replace(",", "");
+        if (request.getHeader("User-Agent").toUpperCase().contains("MSIE") ||
+                request.getHeader("User-Agent").toUpperCase().contains("TRIDENT")
+                || request.getHeader("User-Agent").toUpperCase().contains("EDGE")) {
+            fileName = java.net.URLEncoder.encode(fileName, "UTF-8");
+        } else {
+            //非IE浏览器的处理：
+            fileName = new String(fileName.getBytes("UTF-8"), "ISO-8859-1");
+        }
+        // 通知浏览器进行文件下载
+        response.setContentType(gfsfile.getContentType());
+        response.setHeader("Content-Disposition", "attachment;filename=\"" + fileName + "\"");	
     }
 }
